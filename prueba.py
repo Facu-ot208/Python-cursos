2209e0588cb9e3a2ab438e47ee0ec99c3d5578f7
Nigiriss <xirya641@gmail.com>
2026-07-03 18:10:50 -0300

    Update menu input prompt and print statements

--- prueba.py
+++ prueba.py
@@ -1,17 +1,18 @@
+#primero abrimos el menu principal donde el usuario elegira que hace a continuación
 print ("MENU")
-print("Elija un número")
 print("1.Registrar")
 print("2.Cursos Disp")
 print("3.Inscribirse")
 print("4.Lista de espera")
 print("5.Salir")
-elec = input()
+elec = int(input("Elija una opción: "))
 if elec == 1:
-
+ print("REGISTRO DE UN ALUMNO")
  elif elec == 2:
-
+  print("CURSOS DISPONIBLES")
  elif elec == 3:
-
+  print("INSCRIPCIÓN A CURSOS")
  elif elec == 4:
-
- elif elec == 5 :   
+  print("LISTA DE ESPERA DE CURSOS:")
+ elif elec == 5 :
+  print("GRACIAS POR TU VISITA")
