c854654e0879d338bbdc59666d05646ad7de5eeb
Nigiriss <xirya641@gmail.com>
2026-07-03 16:35:37 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,17 +1,17 @@
 print ("MENU")
 print("Elija un número")
-print("1.Registrar)
+print("1.Registrar")
 print("2.Cursos Disp")
 print("3.Inscribirse")
-print("4.Lista de espera)
+print("4.Lista de espera")
 print("5.Salir")
 elec = input()
 if elec == 1
 
-elif elec == 2
+ elif elec == 2
 
-elif elec == 3
+ elif elec == 3
 
-elif elec == 4
+ elif elec == 4
 
-elif elec == 5    
+ elif elec == 5    
