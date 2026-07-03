ca12f568cf109d9896de2c253afe7abccb1e2ef5
Nigiriss <xirya641@gmail.com>
2026-07-03 16:33:40 -0300

    menu 1

--- prueba.py
+++ prueba.py
@@ -1,5 +1,17 @@
-hola = 12
-if hola > 3:
-    print("hola")
-elif hola < 3
-    print("adios")
+print ("MENU")
+print("Elija un número")
+print("1- Registrar)
+print("2-Cursos Disp")
+print("3-Inscribirse")
+print("4-Lista de espera)
+print("5- Salir")
+elec = input()
+if elec == 1
+
+elif elec == 2
+
+elif elec == 3
+
+elif elec == 4
+
+elif elec == 5    
