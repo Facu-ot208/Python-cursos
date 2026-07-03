5112a8267ed9cc85b5e5c8e520eedf21ffd62e77
Nigiriss <xirya641@gmail.com>
2026-07-03 17:58:19 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,12 +6,12 @@ print("3.Inscribirse")
 print("4.Lista de espera")
 print("5.Salir")
 elec = input()
-if elec == 1
+if elec == 1:
 
- elif elec == 2
+ elif elec == 2:
 
- elif elec == 3
+ elif elec == 3:
 
- elif elec == 4
+ elif elec == 4:
 
- elif elec == 5    
+ elif elec == 5 :   
